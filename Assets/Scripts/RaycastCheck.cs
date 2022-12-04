ef15f182858868a1f0999cbee851727e1641fb74
Gman9000 <granvillejn@aol.com>
2022-12-04 00:31:40 -0500

    raycast check
    raycast check fixed to be more consistent

--- Assets/Scripts/RaycastCheck.cs
+++ Assets/Scripts/RaycastCheck.cs
@@ -4,45 +4,45 @@ using UnityEngine;
 
 public static class RaycastChecks
 {
-    public static bool IsGrounded(this BoxCollider2D collider, Vector2 position, float playerScale)
+    public static bool IsGrounded(this BoxCollider2D collider, Vector2 leftRaycast, Vector2 rightRaycast, float playerScale, float rayCastMagnitude)
     {
         //scale of player is same on x and y
         int layerMask = LayerMask.GetMask("Ground");
 
-        Debug.DrawRay(new Vector2((position.x + 0.98f * (collider.size.x * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), 1.05f * (collider.size.x / 2 * playerScale) * Vector2.down, Color.green, Time.fixedDeltaTime);
-        Debug.DrawRay(new Vector2((position.x - 0.98f * (collider.size.x * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), 1.05f * (collider.size.x / 2 * playerScale) * Vector2.down, Color.green, Time.fixedDeltaTime);
-        return Physics2D.Raycast(new Vector2((position.x + 0.98f * (collider.size.x * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), Vector2.down, 1.05f * (collider.size.x / 2 * playerScale), layerMask) ||
-               Physics2D.Raycast(new Vector2((position.x - 0.98f * (collider.size.x * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), Vector2.down, 1.05f * (collider.size.x / 2 * playerScale), layerMask);
+        Debug.DrawRay(new Vector2(leftRaycast.x, leftRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.down, Color.green, Time.fixedDeltaTime);
+        Debug.DrawRay(new Vector2(rightRaycast.x, rightRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.down, Color.green, Time.fixedDeltaTime);
+        return Physics2D.Raycast(new Vector2(leftRaycast.x, leftRaycast.y), Vector2.down, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask) ||
+               Physics2D.Raycast(new Vector2(rightRaycast.x, rightRaycast.y), Vector2.down, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask);
     }
-    public static bool IsHittingCeiling(this BoxCollider2D collider, Vector2 position, float playerScale)
+    public static bool IsHittingCeiling(this BoxCollider2D collider, Vector2 leftRaycast, Vector2 rightRaycast, float playerScale, float rayCastMagnitude)
     {
         //scale of player is same on x and y
         int layerMask = LayerMask.GetMask("Ground");
 
-        Debug.DrawRay(new Vector2((position.x + 0.98f * (collider.size.x * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), -1.05f * (collider.size.x / 2 * playerScale) * Vector2.down, Color.green, Time.fixedDeltaTime);
-        Debug.DrawRay(new Vector2((position.x - 0.98f * (collider.size.x * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), -1.05f * (collider.size.x / 2 * playerScale) * Vector2.down, Color.green, Time.fixedDeltaTime);
-        return Physics2D.Raycast(new Vector2((position.x + 0.98f * (collider.size.x * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), Vector2.down, -1.05f * (collider.size.x / 2 * playerScale), layerMask) ||
-               Physics2D.Raycast(new Vector2((position.x - 0.98f * (collider.size.x  * playerScale) / 2) - collider.offset.x, collider.offset.y + position.y), Vector2.down, -1.05f * (collider.size.x / 2 * playerScale), layerMask);
+        Debug.DrawRay(new Vector2(leftRaycast.x, leftRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.up, Color.green, Time.fixedDeltaTime);
+        Debug.DrawRay(new Vector2(rightRaycast.x, rightRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.up, Color.green, Time.fixedDeltaTime);
+        return Physics2D.Raycast(new Vector2(leftRaycast.x, leftRaycast.y), Vector2.up, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask) ||
+               Physics2D.Raycast(new Vector2(rightRaycast.x, rightRaycast.y), Vector2.up, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask);
     }
-    public static bool IsHittingRightWall(this BoxCollider2D collider, Vector2 position, float playerScale)
+    public static bool IsHittingRightWall(this BoxCollider2D collider, Vector2 leftRaycast, Vector2 rightRaycast, float playerScale, float rayCastMagnitude)
     {
         //scale of player is same on x and y
         int layerMask = LayerMask.GetMask("Ground");
 
-        Debug.DrawRay(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y + 0.95f * (collider.size.x * playerScale) / 2)), 1.05f * (collider.size.x / 2 * playerScale) * Vector2.right, Color.blue, Time.fixedDeltaTime);
-        Debug.DrawRay(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y - 0.95f * (collider.size.x * playerScale) / 2)), 1.05f * (collider.size.x / 2 * playerScale) * Vector2.right, Color.blue, Time.fixedDeltaTime);
-        return Physics2D.Raycast(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y + 0.95f * (collider.size.x * playerScale) / 2)), Vector2.right, -1.05f * (collider.size.x / 2 * playerScale), layerMask) ||
-               Physics2D.Raycast(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y - 0.95f * (collider.size.x * playerScale) / 2)), Vector2.right, -1.05f * (collider.size.x / 2 * playerScale), layerMask);
+        Debug.DrawRay(new Vector2(leftRaycast.x, leftRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.right, Color.blue, Time.fixedDeltaTime);
+        Debug.DrawRay(new Vector2(rightRaycast.x, rightRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.right, Color.blue, Time.fixedDeltaTime);
+        return Physics2D.Raycast(new Vector2(leftRaycast.x, leftRaycast.y), Vector2.right, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask) ||
+               Physics2D.Raycast(new Vector2(rightRaycast.x, rightRaycast.y), Vector2.right, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask);
     }
 
-    public static bool IsHittingLeftWall(this BoxCollider2D collider, Vector2 position, float playerScale)
+    public static bool IsHittingLeftWall(this BoxCollider2D collider, Vector2 leftRaycast, Vector2 rightRaycast, float playerScale, float rayCastMagnitude)
     {
         //scale of player is same on x and y
         int layerMask = LayerMask.GetMask("Ground");
 
-        Debug.DrawRay(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y + 0.95f * (collider.size.x * playerScale) / 2)), -1.05f * (collider.size.x / 2 * playerScale) * Vector2.right, Color.blue, Time.fixedDeltaTime);
-        Debug.DrawRay(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y - 0.95f * (collider.size.x * playerScale) / 2)), -1.05f * (collider.size.x / 2 * playerScale) * Vector2.right, Color.blue, Time.fixedDeltaTime);
-        return Physics2D.Raycast(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y + 0.95f * (collider.size.x * playerScale) / 2)), Vector2.right, -1.05f * (collider.size.x / 2 * playerScale), layerMask) ||
-               Physics2D.Raycast(new Vector2(position.x + collider.offset.x, collider.offset.y + (position.y - 0.95f * (collider.size.x * playerScale) / 2)), Vector2.right, -1.05f * (collider.size.x / 2 * playerScale), layerMask);
+        Debug.DrawRay(new Vector2(leftRaycast.x, leftRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.left, Color.blue, Time.fixedDeltaTime);
+        Debug.DrawRay(new Vector2(rightRaycast.x, rightRaycast.y), rayCastMagnitude * (collider.size.x / 2 * playerScale) * Vector2.left, Color.blue, Time.fixedDeltaTime);
+        return Physics2D.Raycast(new Vector2(leftRaycast.x, leftRaycast.y), Vector2.left, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask) ||
+               Physics2D.Raycast(new Vector2(rightRaycast.x, rightRaycast.y), Vector2.left, rayCastMagnitude * (collider.size.x / 2 * playerScale), layerMask);
     }
 }
